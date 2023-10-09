800233556ca09dffb349530ba25c7c12a94ab426
1ms20cs003 <1ms20cs003@msrit.edu>
2023-10-10 01:35:05 +0530

    html added

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello_world():
-    return " Whatshellooo skk sapp"
+    return render_template('message.html')  
 
 
 if __name__ == "__main__":
